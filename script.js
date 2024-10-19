75669564421cb3a068fd448d9412dc6d8a670b3e
Gouranga Saha <gourangasaha1206@gmail.com>
2024-10-19 12:39:00 +0530

    Fetch Done in Script.js

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+const form = document.getElementById("submitChanges");
 
-const country = document.getElementById("search")
-const searchButton = document.getElementById("submit")
+form.addEventListener("submit", async function(e){
+    e.preventDefault();
+    const countryInput = document.getElementById('search').value;
+    const displayData = document.getElementById("data");
+    const finalData = await fetchData(countryInput)
+    console.log(finalData);
+    
+    displayData.innerHTML=`
+    <pre>
+    <b>${countryInput}</b>
+    country-code:${finalData.sys.country}
+    temp: ${finalData.main.temp}
+    weather: ${finalData.weather[0].main}
+    min-temp:${finalData.main.temp_min}
+    max-temp:${finalData.main.temp_max}
+    humidity:${finalData.main.humidity}
+    </pre>
+    `
 
-searchButton.addEventListener("click", async function(){
-    const data = document.getElementById('data');
-    const finalData = await fetchData(toString(country.value))
-    data.innerText = finalData
 })
 
+
 async function fetchData(country){
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country},uk&callback=test&appid=bdaeb228384a0b78a57e3eac8536ac4f`, {
-        method : 'POST',
-        headers:{
-            'Content-Type':'application/json'
-        }
-    })
-    const finalData = await response.json();
-    return finalData;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=bdaeb228384a0b78a57e3eac8536ac4f`)
+    let finaldata = await response.json();
+    if(!response.cod === 200){
+        throw new Error("Network Issue")
+    }
+    return finaldata;
 }
